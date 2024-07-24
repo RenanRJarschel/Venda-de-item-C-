75ba61c556c8d933cb01092e686da22143deca88
Renan.Jarschel <renan.jarschel112@gmail.com>
2024-07-24 14:43:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System; 
+
+class Aula06 { 
+
+    static void Main()
+    {
+        int n1, n2, n3;
+
+        n1 = 10; n2 = 20; n3 = 30;
+
+    Console.WriteLine("n1=\t{0}\nn2=\t{1}\nn3=\t{2}\n" ,n1,n2,n3); // "\n" Quebra linha "\t" Insere uma tabulação
+
+
+        double valorCompra = 5.50;
+        double valorVenda;
+        double lucro = 0.2;
+        string produto = "Pastel";
+
+        valorVenda = valorCompra + (valorCompra * lucro);
+
+        Console.WriteLine("Produto.........:{0,15}", produto);
+        Console.WriteLine("Val.Compra......:{0,15:c}", valorCompra); // ":c" valor em real
+        Console.WriteLine("Lucro...........:{0,15:p}", lucro); // ":p" porcentagem 
+        Console.WriteLine("Venda...........:{0,15:c}", valorVenda);
+    }
+
+
+}
